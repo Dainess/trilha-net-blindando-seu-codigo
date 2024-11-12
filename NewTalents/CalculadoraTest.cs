0ee9ecd01583de9301c4177b2a7d9b123d839b6a
Daniel Mittelman <danielcmittelman@gmail.com>
2024-11-12 02:51:35 -0300

    adicionada assinatura dos métodos na classe e todos os testes

--- NewTalents/CalculadoraTest.cs
+++ NewTalents/CalculadoraTest.cs
@@ -4,17 +4,114 @@ namespace NewTalents;
 
 public class CalculadoraTest
 {
-    [Fact]
-    public void TestCurrentValue()
+    private void TestCurrentValue()
     {
         //Arrange
-        double value = 0;
-        Calculadora calculadora = new Calculadora(value);
+        int value = 0;
+        Calculadora calculadora = new(value);
 
         //Act
-        double testValue = calculadora.CurrentValue;
+        int testValue = calculadora.CurrentValue;
 
         //Assert
         Assert.Equal(testValue, value);
+    }
+
+    [Theory]
+    [InlineData(3, 2, 5)]
+    [InlineData(5, -9, -4)]
+    [InlineData(16, 64, 80)]
+    public void TestSomarDoisNumeros(int val1, int val2, int resultadoEsperado)
+    {
+        //Arrange
+        Calculadora calculadora = new();
+
+        //Act
+        int resultado = calculadora.Somar(val1, val2);
+
+        //Asset
+        Assert.Equal(resultadoEsperado, resultado);
+    }
+
+    [Theory]
+    [InlineData(3, 2, 1)]
+    [InlineData(5, -9, -4)]
+    [InlineData(151, 15, 136)]
+    public void TestSubtrairDoisNumeros(int val1, int val2, int resultadoEsperado)
+    {
+        //Arrange
+        Calculadora calculadora = new();
+
+        //Act
+        int resultado = calculadora.Subtrair(val1, val2);
+
+        //Asset
+        Assert.Equal(resultadoEsperado, resultado);
+    }
+
+    [Theory]
+    [InlineData(3, 2, 6)]
+    [InlineData(5, -9, -45)]
+    [InlineData(6, 6, 36)]
+    public void TestMultiplicarDoisNumeros(int val1, int val2, int resultadoEsperado)
+    {
+        //Arrange
+        Calculadora calculadora = new();
+
+        //Act
+        int resultado = calculadora.Multiplicar(val1, val2);
+
+        //Asset
+        Assert.Equal(resultadoEsperado, resultado);
+    }
+
+    [Theory]
+    [InlineData(3, 2, 1)]
+    [InlineData(5, 3, 1)]
+    [InlineData(7, 3, 2)]
+    public void TestDividirDoisNumeros(int val1, int val2, int resultadoEsperado)
+    {
+        //Arrange
+        Calculadora calculadora = new();
+
+        //Act
+        int resultado = calculadora.Dividir(val1, val2);
+
+        //Asset
+        Assert.Equal(resultadoEsperado, resultado);
+    }
+
+    [Fact]
+    public void TestDivisaoPorZero()
+    {
+        //Arrange
+        Calculadora calculadora = new();
+        int val1 = 15, val2 = 0;
+
+        //Act
+        
+
+        //Asset
+        Assert.Throws<DivideByZeroException>(() => calculadora.Dividir(val1, val2));
+
+    }
+
+    [Fact]
+    public void TestHistorico()
+    {
+        //Arrange
+        Calculadora calculadora = new();
+
+        //Act
+        calculadora.Somar(1, 2);
+        calculadora.Somar(5, 8);
+        calculadora.Multiplicar(3, 16);
+        calculadora.Subtrair(15, 21);
+        var lista = calculadora.Historico();
+        var tamanhoEsperadoLista = 3;
+
+        //Asset
+        Assert.Empty(lista);
+        Assert.Equal(tamanhoEsperadoLista, lista.Count);
     }
 }
--- src/Calculadora.cs
+++ src/Calculadora.cs
@@ -1,6 +1,32 @@
 namespace src;
 
-public class Calculadora(double value)
+public class Calculadora(int value)
 {
-    public double CurrentValue { get; set; } = value;
+    public Calculadora() : this(0) {}
+    public int CurrentValue { get; set; } = value;
+
+    public int Somar(int val1, int val2)
+    {
+        return 0;
+    }
+
+    public int Subtrair(int val1, int val2)
+    {
+        return -1;
+    }
+
+    public int Multiplicar(int val1, int val2)
+    {
+        return 0;
+    }
+
+    public int Dividir(int val1, int val2)
+    {
+        return 0;
+    }
+
+    public List<int> Historico()
+    {
+        return [];
+    }
 }
